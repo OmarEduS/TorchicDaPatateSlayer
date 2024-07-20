8095e9993f23a8ddf20680e2cfd8ba54149a8b21
mkuroni <beaudet.lou.sebastien@gmail.com>
2024-07-20 11:36:51 -0400

    init1

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 PORT: int = 10000
 HOST: str = '172.16.10.1'
 #HOST: str = '127.0.0.1'
-ID: str = '07f1d2d3-46ac-11ef-a496-0242ac150002'
+ID: str = 'be8e5a31-46ad-11ef-98ac-0242ac150002'
 
 MAGIC: int = 0x11223344
 SUPER_ADMIN_PORT: int = 10002
